ab298e392c98b6bb3a73f11312c878200394c38f
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-05-11 09:33:16 +0700

    Buoi 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_we16304';
+  title = 'angular_we16304 FPT Poly';
+
+  // Phần logic, định nghĩa giá trị biến và sử dụng ở html
+  name = 'tuannda3';
+  class = 'WE16304';
+
+  // kiểu dữ liệu mảng
+  students = [
+    {
+      id: 'PH11111',
+      name: 'tuannda1',
+      status: 0,
+    },
+    {
+      id: 'PH22222',
+      name: 'tuannda2',
+      status: 1
+    },
+    {
+      id: 'PH33333',
+      name: 'tuannda3',
+      status: 1
+    },
+  ];
 }
