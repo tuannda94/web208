86bc0a8e62480b1f34945da1f010a0047bfc87f5
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-05-11 07:24:00 +0700

    Buoi 1: component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// Đây là nơi định nghĩa component và logic
 export class AppComponent {
-  title = 'angular_we16301';
+  // Định nghĩa các biến,
+  // Các biến này có thể nhận được giá trị ở file html
+  title = 'angular we16301 FPT Poly';
+
+  // Khai báo thêm 1 số biến để html sử dụng
+  name = 'tuannda3';
+  class = 'we16301';
+
+  students = [
+    {
+      name: 'tuannda1',
+      id: 'PH1111'
+    },
+    {
+      name: 'tuannda2',
+      id: 'PH2222'
+    }
+  ];
 }
