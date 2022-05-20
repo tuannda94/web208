49072556337d03cf0f21b13b7bcb321009932a5b
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-05-20 10:38:59 +0700

    CRUD Form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -12,7 +13,51 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(formData: {name: string, age: string}) {
-    console.log(formData);
+  inputValues = {
+    id: 0,
+    name: '123123123',
+    age: 0,
+    email: ''
+  };
+
+  users = [
+    {
+      id: 1,
+      name: 'tuannda3',
+      age: 30,
+      email: 'tuannda3@fe.edu.vn',
+    },
+    {
+      id: 15,
+      name: 'tuannda2',
+      age: 30,
+      email: 'tuannda2@fe.edu.vn',
+    },
+    {
+      id: 10,
+      name: 'tuannda4',
+      age: 30,
+      email: 'tuannda4@fe.edu.vn',
+    },
+  ];
+
+  onSubmit(userForm: NgForm) { // Nhận toàn bộ form
+    //1. Tìm ra id lớn nhất
+    const userIds = this.users
+      .map(user => user.id)
+      .sort((a, b) => a - b); // [1, 10, 15]
+    const newId = userIds[userIds.length - 1];
+    // 2. Thêm vào mảng
+    this.users.push({
+      ...userForm.value, // Lấy ra object giá trị của form
+      id: newId + 1
+    });
+    // 3. Cập nhật lại giá trị ban đầu
+    // userForm.resetForm(); // Nếu không truyền gì thì tất cả về null
+    userForm.resetForm({
+      name: '',
+      age: 0,
+      email: ''
+    });
   }
 }
