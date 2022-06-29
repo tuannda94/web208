08afe1f07f89d18db80b203758f669fe5848b645
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-06-29 11:21:02 +0700

    Buoi 1 + 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_project';
+  content = 'WEB207.01';
+
+  student = {
+    name: 'Nguyen Van A',
+    id: 'PH12345',
+    phone: '0123456789'
+  };
+
+  users = [
+    {
+      name: 'tuannda3',
+      id: 1,
+      status: 0
+    },
+    {
+      name: 'tuannda4',
+      id: 2,
+      status: 1
+    },
+    {
+      name: 'tuannda3',
+      id: 3,
+      status: 0
+    },
+  ];
 }
